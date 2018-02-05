3311cbe05d1e3057e9d10362cd51985c595ed418
Falon Darville <falondarville@gmail.com>
2018-02-05 11:11:21 -0800

    add JQuery styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,34 @@ $(document).ready(function() {
 
 	// initial variables
 	var lisa = {
+		name: Lisa,
 		HP: 180,
-		attackPower: 0,
+		attackPower: 7,
 		counterAttackPower: 25
 	}
 	var denny = {
+		name: Denny,
 		HP: 120,
-		attackPower: 0,
+		attackPower: 6,
 		counterAttackPower: 18
 	}
 	var johnny = {
+		name: Johnny,
 		HP: 100,
-		attackPower: 0,
+		attackPower: 4,
 		counterAttackPower: 12
 	}
 	var mark = {
+		name: Mark,
 		HP: 80,
-		attackPower: 0,
+		attackPower: 3,
 		counterAttackPower: 10
 	}
 	
-	yourCharacter = "";
-	enemiesAvailable = "";
-	yourDefender = "";
-	yourAttackStrength = 0;
+	// yourCharacter = "";
+	// enemiesAvailable = "";
+	// yourDefender = "";
+	// yourAttackStrength = 0;
 
 	// update HP variables
 	$("#lisa-hp").html(lisa.HP);
@@ -42,11 +46,14 @@ $(document).ready(function() {
 		$(this).parent().find(".restyle").css("background-color", "#83C1BC");
 		$(this).css("background-color", "#74D774");
 		$(this).parent().find(".restyle").appendTo(".enemies-available");
-		$(this).appendTo(".your-character").css("float", "none");
+		var yourCharacter = $(this).appendTo(".your-character").css("float", "none");
 		clicks = false;
 		} else {
-			$(this).appendTo(".defender").css("background-color","#FFB6C1");
+			var yourDefender = $(this).appendTo(".defender").css("background-color","#FFB6C1");
 		}
+		// return yourCharacter;
+		// return yourDefender;
+		// how do I grab the id of the character and defenders
 	});
 
 	// Attack button, prints message
@@ -57,5 +64,14 @@ $(document).ready(function() {
 
 
 
+	// if attack button clicked and there is no enemy selected, display text "No enemy here."
+
+	// if your characters reaches 0 HP, you lose
+
+	// if your defenders reaches 0 HP, they are deleted from the game
+
+	// you win when you beat all three enemies
+
+	// reset game button appears when you lose against an enemy or you win against all three enemies
 
 });
