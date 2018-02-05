024f9a5930c4f1bc43e1d1cf97662ea71710d0da
Falon Darville <falondarville@gmail.com>
2018-02-05 10:24:24 -0800

    card movement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,61 @@
-// initial variables
-var lisaHP = 180;
-var dennyHP = 120;
-var johnnyHP = 100;
-var markHP = 80;
-yourCharacter = "";
-enemiesAvailable = "";
-yourDefender = "";
-yourAttackStrength = 0;
+$(document).ready(function() {
 
-// update HP variables
-$("#lisa-hp").html(lisaHP);
-$("#denny-hp").html(dennyHP);
-$("#johnny-hp").html(johnnyHP);
-$("#mark-hp").html(markHP);
+	// initial variables
+	var lisa = {
+		HP: 180,
+		attackPower: 0,
+		counterAttackPower: 25
+	}
+	var denny = {
+		HP: 120,
+		attackPower: 0,
+		counterAttackPower: 18
+	}
+	var johnny = {
+		HP: 100,
+		attackPower: 0,
+		counterAttackPower: 12
+	}
+	var mark = {
+		HP: 80,
+		attackPower: 0,
+		counterAttackPower: 10
+	}
+	
+	yourCharacter = "";
+	enemiesAvailable = "";
+	yourDefender = "";
+	yourAttackStrength = 0;
 
-// choose character, change background color, move to Your Character section
+	// update HP variables
+	$("#lisa-hp").html(lisa.HP);
+	$("#denny-hp").html(denny.HP);
+	$("#johnny-hp").html(johnny.HP);
+	$("#mark-hp").html(mark.HP);
 
-// move other three characters to Enemies section, change background color
+	// choose character, change background color, move to Your Character section
+	// move other three characters to Enemies section, change background color
+	// choose enemy, enemy goes to Defender section
+	var clicks = true;
+	$(".restyle").click(function(){
+		if (clicks) {
+		$(this).parent().find(".restyle").css("background-color", "#83C1BC");
+		$(this).css("background-color", "#74D774");
+		$(this).parent().find(".restyle").appendTo(".enemies-available");
+		$(this).appendTo(".your-character").css("float", "none");
+		clicks = false;
+		} else {
+			$(this).appendTo(".defender").css("background-color","#FFB6C1");
+		}
+	});
 
-// choose enemy, enemy goes to Defender section
+	// Attack button, prints message
 
-// Attack button, prints message
+	$(".attack-button").click(function() {
+		$(".printActions").text("You attacked " + yourDefender + " for " +yourAttackStrength + " damage. " + yourDefender + " attacked you back for " + "fill this" + " damage. ");
+	});
 
-$(".attack-button").click(function() {
-	$(".print-actions").text("You attacked " + yourDefender + " for " yourAttackStrength + " damage. " + yourDefender " attacked you back for " + ____ + " damage. "
+
+
+
 });
