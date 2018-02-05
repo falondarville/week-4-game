d5a0560c6d79e5e79acd8006366968f3d64bd483
Falon Darville <falondarville@gmail.com>
2018-02-04 20:50:11 -0800

    character cards

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// initial variables
+var lisaHP = 180;
+var dennyHP = 120;
+var johnnyHP = 100;
+var markHP = 80;
+
+// update HP variables
+$("#lisa-hp").html(lisaHP);
+$("#denny-hp").html(dennyHP);
+$("#johnny-hp").html(johnnyHP);
+$("#mark-hp").html(markHP);
+
+// dynamically create the characters
