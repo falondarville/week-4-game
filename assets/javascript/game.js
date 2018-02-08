d290132e50d0e7d5d63d46ffb5ea3bad03e5a2e5
Falon Darville <falondarville@gmail.com>
2018-02-07 21:30:23 -0800

    minor mods

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -40,16 +40,16 @@ $(document).ready(function() {
 	// move other three characters to Enemies section, change background color
 	// choose enemy, enemy goes to Defender section
 	var clicks = true;
-	$(".restyle").click(function(){
+	$(".restyle").click(function() {
 		if (clicks) {
+
 		$(this).parent().find(".restyle").css("background-color", "#83C1BC");
 		$(this).css("background-color", "#74D774");
 		$(this).parent().find(".restyle").appendTo(".enemies-available");
 		$(this).appendTo(".your-character").css("float", "none");
+
 		// how do I grab the id of the character and defenders? I actually may not need to do this. Instead, I need to be able to refer to the variables I initially set out for each character. This will mean that I can reach into the variable to grab additional information I need further in the game (starting HP, attack power, and counter attack power)
 		// alternatively, I can assign an id to the character moved to new div
-		// this didn't work: var yourCharacter = $(".your-character").children().first().attr("id");
-		// this didn't work: console.log($(".your-character option:selected").attr("id"));
 		clicks = false;
 		} else {
 			$(this).appendTo(".defender").css("background-color","#FFB6C1");
@@ -64,27 +64,27 @@ $(document).ready(function() {
 
 	// CALCULATE FIGHTING: will this be a separate function altogether or part of the attack-button function below? It is dependent on click, so it may come down to game design, and the functionality can work either way as long as I call the calculations withing the click function. 
 
-	// Attack button, prints message
-	$(".attack-button").click(function() {
-		// if your characters reaches 0 HP, you lose
-		if (your-character.HP <= 0) {
-			$(".printActions").text("You lost.");
-			// show reset game button
-			// game over
-		} else if (yourDefender.HP <= 0) {
-			$(".printActions").text("You defeated " + yourDefender + " .");
-			// show reset game button
-			// defenders is deleted from the game
-			// check if there are any other enemies available (this may have to be a different function including a counter of three total enemies), and if there are, prompt user to choose another enemy. If there are no other enemies, the user wins the whole game. 
-		} else if () {
-			// if attack button clicked and there is no enemy selected, display text "No enemy here."
-		}
+	// // Attack button, prints message
+	// $(".attack-button").click(function() {
+	// 	// if your characters reaches 0 HP, you lose
+	// 	if (your-character.HP <= 0) {
+	// 		$(".printActions").text("You lost.");
+	// 		// show reset game button
+	// 		// game over
+	// 	} else if (yourDefender.HP <= 0) {
+	// 		$(".printActions").text("You defeated " + yourDefender + " .");
+	// 		// show reset game button
+	// 		// defenders is deleted from the game
+	// 		// check if there are any other enemies available (this may have to be a different function including a counter of three total enemies), and if there are, prompt user to choose another enemy. If there are no other enemies, the user wins the whole game. 
+	// 	} else if () {
+	// 		// if attack button clicked and there is no enemy selected, display text "No enemy here."
+	// 	}
 
-		$(".printActions").text("You attacked " + yourDefender + " for " + lisa.attackPower + " damage. " + yourDefender + " attacked you back for " + "fill this" + " damage. ");
-		// placeholder functionality
-		attackpower +=;
-		HP -= yourDefender.counterAttackPower;
+	// 	$(".printActions").text("You attacked " + yourDefender + " for " + lisa.attackPower + " damage. " + yourDefender + " attacked you back for " + "fill this" + " damage. ");
+	// 	// placeholder functionality
+	// 	attackpower +=;
+	// 	HP -= yourDefender.counterAttackPower;
 
-	});
+	// });
 
 });
